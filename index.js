bd1ab31ccfb12946abc86fdb8e5d9ccf8fbc3bde
Sodiq Farhan <sodiqfarhan360@gmail.com>
2022-09-22 10:59:56 +0100

    Do step8 of the phonebook backend task of part3

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const morgan = require('morgan');
 
 const app = express();
 app.use(express.json());
-app.use(morgan());
+
+morgan.token('body', req => {
+    if(req.method === 'POST'){
+        return JSON.stringify(req.body)
+    }
+})
+  
+app.use(morgan(':method :url :status :res[content-length] - :body'));
 
 let contacts = [
     { 
